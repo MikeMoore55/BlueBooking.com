8cef3f66cbbfb103f769c29346c6ea90a6c80f93
MikeMoore55 <michaelwillemmoore@gmail.com>
2022-06-22 14:55:24 +0200

    made one temporary stylesheet
    made one temporary stylesheet because i am having errors

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Kdam+Thmor+Pro&display=swap" rel="stylesheet">
     <!-- css stylesheet -->
-    <link rel="stylesheet" href="./src/public/css/main.css">
+    <link rel="stylesheet" href="./src/public/css/stylesheet.css">
 </head>
 <body>
     <?php
