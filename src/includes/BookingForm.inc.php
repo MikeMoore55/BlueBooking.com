f81fdf8b44c72987f8435dbe031e66eca12adf47
MikeMoore55 <michaelwillemmoore@gmail.com>
2022-06-23 20:25:41 +0200

    small fix

--- src/includes/BookingForm.inc.php
+++ src/includes/BookingForm.inc.php
@@ -3,7 +3,6 @@
     function createBookingFormOption($Hotels)
     {
         foreach ($Hotels as $index => $HotelsArray) {
-            $index = $index + 1;
             echo '
                     <option>'.$HotelsArray["name"].'</option>
                 ';
