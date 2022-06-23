4d6fa8a170b1c0d5a6a788a76512b1ff566dfc64
MikeMoore55 <michaelwillemmoore@gmail.com>
2022-06-23 08:17:38 +0200

    displayed comparison
    display comparison as well as original booking

--- src/includes/CompareBooking.inc.php
+++ src/includes/CompareBooking.inc.php
@@ -16,11 +16,51 @@
     unset($hotelOptionArray["$selectedHotelName"]);
 
     $options = $hotelOptionArray;
-    $twoOptions = array_slice($options, 0, 2);
+    $alternativeOptions = array_slice($options, 0, 2);
 ?>
 
 <main>
-    <?php
-        print_r($twoOptions);
-    ?>
+    <div class="compare-info">
+
+        <fieldset class="original-booking">
+            <legend>Your Booking</legend>
+            <?php
+                foreach ($BookingArray as $BookingInfo => $Booking) {
+                    echo 
+                        '
+                            <h2>'.$Booking["hotel"].'</h2>
+                            <img class="hotel-img" src="'.$Booking["image"].'"
+                            <p><span>Rate :</span>'.$Booking["rate"].'</p>
+                            <p>'.$Booking["rating"].'<img class="rating-img" src="./src/public/images/star.png"></p>
+                            <h3>Amenities: </h3>
+                            <ul>
+                                <li>Pool: '.$Booking["pool"].'</li>
+                                <li>Spa: '.$Booking["spa"].'</li>
+                                <li>Restaurant: '.$Booking["restaurant"].'</li>
+                                <li>Child-Friendly: '.$Booking["childFriendly"].'</li>
+                            </ul>
+                        ';
+                }
+            ?>
+        </fieldset>
+        <?php
+            foreach ($alternativeOptions as $option => $value) {
+                echo '
+                    <div class="alternative-booking">
+                        <h2>'.$option.'</h2>
+                        <p>Rate:'.$value["rate"].'</p>
+                        <p>'.$value["rating"].'<img class="rating-img" src="./src/public/images/star.png"></p>
+                        <ul>
+                            <li>Pool: '.$value["pool"].'</li>
+                            <li>Spa: '.$value["spa"].'</li>
+                            <li>Restaurant: '.$value["restaurant"].'</li>
+                            <li>ChildFriendly: '.$value["childFriendly"].'</li>
+                        </ul>
+                    </div>
+                    ';
+            }
+        ?>
+
+    </div>
+   
 </main>
