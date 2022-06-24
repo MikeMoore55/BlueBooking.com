afb69ba51cedfd5906bf4d8de5412d36ea011994
MikeMoore55 <michaelwillemmoore@gmail.com>
2022-06-24 07:08:48 +0200

    comments added to functions

--- src/functions/calcCost.func.php
+++ src/functions/calcCost.func.php
@@ -1,7 +1,8 @@
 <?php
+    /* calculate total costs for the stay */
     function calcCosts($days, $rate){
         $totalCosts =  $days * $rate;
         
         return $totalCosts;
-    }
+    } // future update - make it calculate with different rates depending on the dates in season ie. winter rates < summer rates
 ?>
--- src/functions/calcDays.func.php
+++ src/functions/calcDays.func.php
@@ -1,8 +1,9 @@
 <?php
+    /* calculate the amount of days user is staying */
     function calcDays($checkOut, $checkIn){
         $dayDifference = date_diff(date_create($checkIn), date_create($checkOut));
         $amountOfDays = $dayDifference->format('%a');
 
         return $amountOfDays;
-    };
+    };// future update - make it calculate nights instead of days
 ?>
