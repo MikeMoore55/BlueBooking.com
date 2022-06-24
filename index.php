727549d2f9a1fe98b3d1a5406191001183967231
MikeMoore55 <michaelwillemmoore@gmail.com>
2022-06-24 16:44:11 +0200

    fixing stylesheet problems
    last commit (i hope)

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Kdam+Thmor+Pro&display=swap" rel="stylesheet">
     <!-- css stylesheet -->
-    <link rel="stylesheet" href="./src/public/css/stylesheet.css">
+    <link rel="stylesheet" href="./src/public/css/MainStylesheet.css">
 </head>
 <body>
     <?php
